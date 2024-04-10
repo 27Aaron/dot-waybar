f70e02130ceb1443b33411188c346e98a9ea22e8
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-04-10 14:33:38 +0800

    feat: update launch.sh

--- waybar/launch.sh
+++ waybar/launch.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+# ----------------------------------------------------- 
+# Quit all running waybar instances
+# ----------------------------------------------------- 
+pkill waybar
+sleep 0.2
+
+# ----------------------------------------------------- 
 # Load the configuration
+# ----------------------------------------------------- 
 waybar -c $HOME/.config/waybar/config.json -s $HOME/.config/waybar/style.css &
